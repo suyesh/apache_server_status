f25b01ffe4459a59da2f90808babc45ad794fc66
Suyesh Bhandari <suyeshb@gmail.com>
2017-03-18 01:45:20 -0400

    complete realtime

--- ass/app.rb
+++ ass/app.rb
@@ -7,6 +7,12 @@ require 'bundler/setup'
 require 'nokogiri'
 require 'open-uri'
 
+helpers do
+  def partial(page, options={})
+    erb page.to_sym, options.merge!(:layout => false)
+  end
+end
+
 def set_server(server)
     if server == 'app01'
         'http://dot55simsapp01.dot.nycnet/server-status'
@@ -18,36 +24,29 @@ def set_server(server)
 end
 
 get '/' do
-    url = 'https://simstcdstg.net/server-status'
-    doc = Nokogiri::HTML(open(url))
     @current_app = 'SimsTcd'
-    @cpu = doc.css('dl')[1].children[15].text.split[7][0..-2]
-    @load_average = doc.css('dl')[1].children[11].text.split[2]
-    @requests_per_second = doc.css('dl')[1].children[17].text.split[0]
-    @requests = doc.css('dl')[1].children[19].text.split[0]
-    @idle_workers = doc.css('dl')[1].children[19].text.split[5]
-    @size_per_request = doc.css('dl')[1].children[17].text.split[6]
-    @size_title = doc.css('dl')[1].children[17].text.split[7]
     erb :index
 end
 
 get '/app01' do
-  erb :app01
+    @current_app = 'APP01'
+    erb :app01
 end
 
 get '/app02' do
-  erb :app02
+    @current_app = 'APP02'
+    erb :app02
 end
 
-get '/:server' do
+get '/stat/:server' do
     url = set_server(params[:server])
     @current_app = params[:server].upcase
     doc = Nokogiri::HTML(open(url))
     json(cpu: doc.css('dl')[1].children[15].text.split[7][0..-2],
-          load_average: doc.css('dl')[1].children[11].text.split[2],
-          requests_per_second: doc.css('dl')[1].children[17].text.split[0],
-          requests: doc.css('dl')[1].children[19].text.split[0],
-          idle_workers: doc.css('dl')[1].children[19].text.split[5],
-          size_per_request: doc.css('dl')[1].children[17].text.split[6],
-          size_title: doc.css('dl')[1].children[17].text.split[7])
+         load_average: doc.css('dl')[1].children[11].text.split[2],
+         requests_per_second: doc.css('dl')[1].children[17].text.split[0],
+         requests: doc.css('dl')[1].children[19].text.split[0],
+         idle_workers: doc.css('dl')[1].children[19].text.split[5],
+         size_per_request: doc.css('dl')[1].children[17].text.split[6],
+         size_title: doc.css('dl')[1].children[17].text.split[7])
 end
