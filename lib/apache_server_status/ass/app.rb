f5615eca597b1fe769ce7ad35a5ab5b1b49860a3
Suyesh Bhandari <suyeshb@gmail.com>
2017-03-18 14:34:36 -0400

    latest change

--- lib/apache_server_status/ass/app.rb
+++ lib/apache_server_status/ass/app.rb
@@ -1,13 +1,46 @@
+require 'rubygems'
 require 'sinatra'
-r#equire 'apache_server_status'
+require 'sinatra/base'
+require 'sinatra/contrib'
+require 'sinatra/json'
+require 'bundler/setup'
+require 'nokogiri'
+require 'open-uri'
 
-class Ass < Sinatra::Base
-    set :static, true
-    set :public, File.expand_path('..', __FILE__)
-
-    set :views,  File.expand_path('../views', __FILE__)
-
-    get '/' do
-        erb :'/index'
+def set_server(server)
+    if server == 'app01'
+        'http://dot55simsapp01.dot.nycnet/server-status'
+    elsif server == 'app02'
+        'http://dot55simsapp02.dot.nycnet/server-status'
+    else
+        'https://simstcdstg.net/server-status'
     end
+end
+
+get '/' do
+    @current_app = 'SimsTcd'
+    erb :index
+end
+
+get '/app01' do
+    @current_app = 'APP01'
+    erb :app01
+end
+
+get '/app02' do
+    @current_app = 'APP02'
+    erb :app02
+end
+
+get '/stat/:server' do
+    url = set_server(params[:server])
+    @current_app = params[:server].upcase
+    doc = Nokogiri::HTML(open(url))
+    json(cpu: doc.css('dl')[1].children[15].text.split[7][0..-2],
+         load_average: doc.css('dl')[1].children[11].text.split[2],
+         requests_per_second: doc.css('dl')[1].children[17].text.split[0],
+         requests: doc.css('dl')[1].children[19].text.split[0],
+         idle_workers: doc.css('dl')[1].children[19].text.split[5],
+         size_per_request: doc.css('dl')[1].children[17].text.split[6],
+         size_title: doc.css('dl')[1].children[17].text.split[7])
 end
