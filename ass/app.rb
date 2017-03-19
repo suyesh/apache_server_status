151b5ac7b1476d672b525aecc50a8b71c7c7fc0e
Suyesh Bhandari <suyeshb@gmail.com>
2017-03-19 10:17:08 -0400

    Added main.js

--- ass/app.rb
+++ ass/app.rb
@@ -12,27 +12,27 @@ end
 
 get '/:server' do
     begin
-             begin
-                 url = "http://#{params[:server]}/server-status"
-                 doc = Nokogiri::HTML(open(url))
-             rescue
-                 url = "https://#{params[:server]}/server-status"
-                 doc = Nokogiri::HTML(open(url))
-             end
-             json(cpu: doc.css('dl')[1].children[15].text.split[7][0..-2],
-                  load_average: doc.css('dl')[1].children[11].text.split[2],
-                  requests_per_second: doc.css('dl')[1].children[17].text.split[0],
-                  requests: doc.css('dl')[1].children[19].text.split[0],
-                  idle_workers: doc.css('dl')[1].children[19].text.split[5],
-                  size_per_request: doc.css('dl')[1].children[17].text.split[6],
-                  size_title: doc.css('dl')[1].children[17].text.split[7])
+        begin
+            url = "http://#{params[:server]}/server-status"
+            doc = Nokogiri::HTML(open(url))
+        rescue
+            url = "https://#{params[:server]}/server-status"
+            doc = Nokogiri::HTML(open(url))
+        end
+        json(cpu: doc.css('dl')[1].children[15].text.split[7][0..-2],
+             load_average: doc.css('dl')[1].children[11].text.split[2],
+             requests_per_second: doc.css('dl')[1].children[17].text.split[0],
+             requests: doc.css('dl')[1].children[19].text.split[0],
+             idle_workers: doc.css('dl')[1].children[19].text.split[5],
+             size_per_request: doc.css('dl')[1].children[17].text.split[6],
+             size_title: doc.css('dl')[1].children[17].text.split[7])
     rescue
-      json(cpu: "--/--",
-           load_average: "--/--",
-           requests_per_second: "--/--",
-           requests: "--/--",
-           idle_workers: "--/--",
-           size_per_request: "--/--",
-           size_title: "--/--")
+        json(cpu: '--/--',
+             load_average: '--/--',
+             requests_per_second: '--/--',
+             requests: '--/--',
+             idle_workers: '--/--',
+             size_per_request: '--/--',
+             size_title: '--/--')
     end
 end
